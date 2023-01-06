7d35d7606544038a709853c02f808382d5c22862
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2023-01-06 12:48:50 +0100

    Updated the theme

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import Blockly from 'blockly';
 
-var workspace = Blockly.inject('workspace', { toolbox: document.getElementById('toolbox-categories') });
+Blockly.Themes.Modern = Blockly.Theme.defineTheme('halloween', {
+    'base': Blockly.Themes.Classic,
+    'componentStyles': {
+        'workspaceBackgroundColour': '#fff',
+        'toolboxBackgroundColour': '#ccc',
+        'toolboxForegroundColour': '#fff',
+        'flyoutBackgroundColour': '#ddd',
+        'flyoutForegroundColour': '#ccc',
+        'flyoutOpacity': 1,
+        'scrollbarColour': '#333',
+        'insertionMarkerColour': '#fff',
+        'insertionMarkerOpacity': 0.3,
+        'scrollbarOpacity': 0.4,
+        'cursorColour': '#d0d0d0',
+        'blackBackground': '#333'
+      }
+});
+
+
+
+var workspace = Blockly.inject('workspace', {
+    toolbox: document.getElementById('toolbox-categories'),
+    theme: Blockly.Themes.Modern
+});
 
 const onresize = function () { Blockly.svgResize(workspace); };
 window.addEventListener('resize', onresize, false);
 onresize();
-
